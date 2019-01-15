9ba9ec04a3bd2d349b3e195357992a5998b1d81f
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-15 20:15:42 +0000

    added random key generator

--- expressServer.js
+++ expressServer.js
@@ -5,11 +5,18 @@ let  urlDatabase = [
 {"b2xVn2": "http://www.lighthouselabs.ca"},
 {"9sm5xK": "http://www.google.com" }
 ];
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urlsNew");
 });
 
 app.get("/urls", (req, res) => {
@@ -25,13 +32,18 @@ app.get("/urls/:id", (req, res) => {
   res.render("urlsShow", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body><b>Hello World</b></body></html>\n");
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body><b>Hello World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -48,3 +60,9 @@ function findObj(objArray, key){
 
   return keyIndex;
 }
+
+function urlGeneration(){
+  randomNumber =  Math.floor(100000000 + Math.random() * 90000000);
+  return randomNumber.toString(36);
+}
+
